29aef6f61b9b953dd416d8b1f36c5bb01e1015a3
Christian Lopez <christian.lopez023@mymdc.net>
2018-11-27 17:38:57 -0500

    added calculations

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,21 +1,56 @@
 import React, { Component } from "react";
 import Numbers from "./Numbers";
+import Equations from "./Equations";
 
 class Calculator extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            expression: [],
+            evaluation: 0
+        }
+    }
+
+    clearCalculation = () => {
+        this.setState({
+            expression: [],
+            evaluation: 0
+        })
+        console.log(this.state.expression)
+    }
+
+    evaluateExpression = () => {
+        const total = this.state.evaluation;
+        this.setState({
+            evaluation: eval(this.state.expression.join(""))
+        })
+        console.log(total);
     }
     render(){
         const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."];
         const mappedNumbers = numbers.map(number => {
             return(
-                <Numbers num={number} />
+                <Numbers num={number} expression={this.state.expression}/>
+            )
+        })
+
+        const equations = ["+", "-", "*", "/"];
+        const mappedEquations = equations.map(equation => {
+            return(
+                <Equations symbol={equation} expression={this.state.expression} />
             )
         })
         return(
-            <div class="calculator">
-                <div class="numbers">
+            <div className="calculator">
+                <div className="numbers">
                     { mappedNumbers }
+                </div>
+                <div className="symbols">
+                    <button onClick={this.clearCalculation}>C</button>
+                    <div className="equations">
+                        { mappedEquations }
+                    </div>
+                    <button onClick={this.evaluateExpression}>=</button>
                 </div>
             </div>
         )
