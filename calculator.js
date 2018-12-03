df6e22eae3d5ee673e98e849d6d31881f7d9693e
Christian Lopez <christian.lopez023@mymdc.net>
2018-12-02 20:39:25 -0500

    fixed some bugs and reorganized code into functions

--- calculator.js
+++ calculator.js
@@ -6,24 +6,53 @@ const reset = document.getElementById("reset");
 const expressionValues = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "+", "-", "*", "/"];
 let expression = [];
 
+const mathCalc = arrItem => {
+    if(typeof expressionValues[arrItem] !== "number"){
+        checkExpressionLength();
+        disableCalculations();
+    }else {
+        enableCalculations();
+    }
+    expression.push(expressionValues[arrItem]);
+    result.textContent = expression.join("");
+}
 
-equals.addEventListener("click", () => {
-    const total = eval(expression.join(""));
-    result.textContent = total;
-})
+const checkExpressionLength = () => {
+    if(expression.length === 0){
+        expression.unshift(0);
+    }
+}
 
-reset.addEventListener("click", () => {
+const disableCalculations = () => {
+    expressionButtons[11].disabled = true;
+    expressionButtons[13].disabled = true;
+    expressionButtons[14].disabled = true;
+    equals.disabled = true;
+}
+
+const enableCalculations = () => {
+    expressionButtons[11].disabled = false;
+    expressionButtons[13].disabled = false;
+    expressionButtons[14].disabled = false;
+    equals.disabled = false;
+}
+
+const calculations = () => {
+    for(let i = 0; i < expressionValues.length; i++){
+        expressionButtons[i].addEventListener("click", () => mathCalc(i));
+    }
+}
+
+const calcTotal = () => {
+    result.textContent = eval(expression.join(""));
+}
+
+const resetCalculation = () => {
     expression = [];
     result.textContent = 0;
-})
-
-for(let i = 0; i < expressionValues.length; i++){
-    expressionButtons[i].addEventListener("click", () => {
-        // if(typeof expressionValues[expressionValues.length - 2] !== "number"){
-        //     // if(expressionValues[])
-        // }
-        expression.push(expressionValues[i]);
-        result.textContent = expression.join("");
-        console.log(expression);
-    })
 }
+
+equals.addEventListener("click", calcTotal);
+reset.addEventListener("click", resetCalculation);
+
+calculations();
